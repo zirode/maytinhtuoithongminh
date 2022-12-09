f6fe01e070d51871a4330c160b0b7babdfb5f8d7
Huy Lam <huylam.contacts@disroot.org>
2022-12-09 18:44:18 +0700

    idk

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+
+
+function ageProcess() {
+    var ageInput = document.getElementById("ageinput").value;
+
+    if(ageInput==""){
+        document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">Khong duoc de rong</div>'
+    }else if(ageInput<=0){
+        document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">May chua ra doi thi lam gi tao tinh?</div>'
+    }else if(ageInput<=118){
+        document.getElementById("age-result").innerHTML = '<div class="alert alert-success">Tuoi cua ban la: '+ ageInput+'</div>'
+    } else {
+        document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">Lam deo gi co ai ' + ageInput + ' tuoi</div>';
+    }
+}

762016bcd5a0597a6cdb5ce6ff71218224b4dcb1
Phan Huy Lâm <118982577+zirode@users.noreply.github.com>
2022-12-09 18:54:53 +0700

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -7,7 +7,7 @@ function ageProcess() {
         document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">Khong duoc de rong</div>'
     }else if(ageInput<=0){
         document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">May chua ra doi thi lam gi tao tinh?</div>'
-    }else if(ageInput<=118){
+    }else if(ageInput<=119){
         document.getElementById("age-result").innerHTML = '<div class="alert alert-success">Tuoi cua ban la: '+ ageInput+'</div>'
     } else {
         document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">Lam deo gi co ai ' + ageInput + ' tuoi</div>';

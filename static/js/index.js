6b05936d852ddfa34b542fa692b7866d91bc57bb
Phan Huy Lâm <118982577+zirode@users.noreply.github.com>
2022-12-10 04:45:01 +0700

    idk

--- static/js/index.js
+++ static/js/index.js
@@ -4,12 +4,12 @@ function ageProcess() {
     var ageInput = document.getElementById("ageinput").value;
 
     if(ageInput==""){
-        document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">Khong duoc de rong</div>'
+        document.getElementById("age-result").innerHTML = '<div class="result result-danger">Khong duoc de rong</div>'
     }else if(ageInput<=0){
-        document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">May chua ra doi thi lam gi tao tinh?</div>'
+        document.getElementById("age-result").innerHTML = '<div class="result result-danger">May chua ra doi thi lam gi tao tinh?</div>'
     }else if(ageInput<=122){
-        document.getElementById("age-result").innerHTML = '<div class="alert alert-success">Tuoi cua ban la: '+ ageInput+'</div>'
+        document.getElementById("age-result").innerHTML = '<div class="result result-success">Tuoi cua ban la: '+ ageInput+'</div>'
     } else {
-        document.getElementById("age-result").innerHTML = '<div class="alert alert-danger">Lam deo gi co ai ' + ageInput + ' tuoi</div>';
+        document.getElementById("age-result").innerHTML = '<div class="result result-danger">Lam deo gi co ai ' + ageInput + ' tuoi</div>';
     }
 }
